da24d4d7e10229315df79276c96f13e2f428046d
anonymous298 <tackletalha@gmail.com>
2025-03-09 00:31:50 +0500

    initializing Nvidia NIM llm

---
+++ Nvidia-NIM/test.py
@@ -0,0 +1,25 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ['NVIDIA_NIM_API_KEY'] = os.getenv('NVIDIA_NIM_API_KEY')
+
+client = OpenAI(
+  base_url = "https://integrate.api.nvidia.com/v1",
+  api_key = os.getenv('NVIDIA_NIM_API_KEY')
+)
+
+completion = client.chat.completions.create(
+  model="meta/llama-3.3-70b-instruct",
+  messages=[{"role":"user","content":"Hi, How are you?"}],
+  temperature=0.2,
+  top_p=0.7,
+  max_tokens=1024,
+  stream=True
+)
+
+for chunk in completion:
+  if chunk.choices[0].delta.content is not None:
+    print(chunk.choices[0].delta.content, end="")
